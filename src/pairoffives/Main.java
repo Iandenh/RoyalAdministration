484f31ea72bdfb025fdbf6e628d2ad3098630393
Ian den Hartog <iandenhartog@hotmail.com>
2015-12-16 19:29:26 +0100

    add logo in titlebar

--- src/pairoffives/Main.java
+++ src/pairoffives/Main.java
@@ -1,18 +1,25 @@
 
 package pairoffives;
 
+import javax.swing.ImageIcon;
+
+import java.awt.Toolkit;
+import javax.swing.ImageIcon;
+
 public class Main {
 
     /**
      * @main
      */
     public static void main(String[] args) {
-        
+        System.out.println("");
        //Nieuw object "Hoofdscherm" 
         Hoofdscherm hoofdscherm = new Hoofdscherm();
+        
+        java.net.URL imageUrl = Main.class.getResource("/pairoffives/images/32.png");
+        hoofdscherm.setIconImage(new ImageIcon(imageUrl).getImage());
         hoofdscherm.setTitle("Full House");
         hoofdscherm.setVisible(true);
-        
         
         
         
